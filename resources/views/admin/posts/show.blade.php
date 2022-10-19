18a829d17fecc097b53c9e5a6d840a4aa070942f
spellcry <sangiothebest@hotmail.com>
2022-10-19 17:40:32 +0200

    fix posts show content

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -29,7 +29,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                {{ $post->content }}
+                {!! $post->content !!}
             </div>
         </div>
     </div>
