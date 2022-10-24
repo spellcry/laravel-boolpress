5c8bbd26bdd23b34ce0e8022d042f5b743bdda9e
spellcry <sangiothebest@hotmail.com>
2022-10-24 16:52:12 +0200

    add relationship post_tag

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -4,6 +4,7 @@ use App\Category;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Post;
+use App\Tag;
 
 class PostSeeder extends Seeder
 {
@@ -15,14 +16,18 @@ class PostSeeder extends Seeder
     public function run(Faker $faker)
     {
         $categoryIds = Category::all()->pluck('id');
+        $tags = Tag::all()->pluck('id');
         for ($i = 0; $i < 50; $i++) {
             $post = new Post();
             $post->title = $faker->words( rand(5, 10), true);
             $post->content = $faker->paragraphs( rand(10, 20), true);
             $post->slug = str_replace(' ', '-', $post->title);
             $post->category_id = $faker->optional()->randomElement($categoryIds);
-
+            
             $post->save();
+
+            $tagIds = $tags->shuffle()->take(3)->all();
+            $post->tags()->sync($tagIds);
         }
     }
 }
