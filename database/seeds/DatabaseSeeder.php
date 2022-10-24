140c1e0ac53a91d4d454951249a32ab66a45b15a
spellcry <sangiothebest@hotmail.com>
2022-10-24 17:54:25 +0200

    fix DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(TagSeeder::class);
-<<<<<<< HEAD
         $this->call(CategorySeeder::class);
         $this->call(PostSeeder::class);
-=======
-        $this->call(PostSeeder::class);
-        $this->call(CategorySeeder::class);
->>>>>>> 5c8bbd26bdd23b34ce0e8022d042f5b743bdda9e
     }
 }
