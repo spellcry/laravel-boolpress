07924379fe524743a0fedda877f2ffab62703645
spellcry <sangiothebest@hotmail.com>
2022-10-26 16:02:34 +0200

    hot fix table post_tag

--- database/migrations/2022_10_24_143452_create_post_tag_table.php
+++ database/migrations/2022_10_24_143452_create_post_tag_table.php
@@ -15,10 +15,10 @@ class CreatePostTagTable extends Migration
     {
         Schema::create('post_tag', function (Blueprint $table) {
             $table->unsignedBigInteger('post_id');
-            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
             
             $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
 
             $table->primary(['post_id','tag_id']);
         });
